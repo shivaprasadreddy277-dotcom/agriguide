679b5ff39db4974cc2043548719131a6fe5fabff
shivaprasadreddy277-dotcom <shivaprasadreddy277@gmail.com>
2026-08-27 15:29:32 +0530

    feat: configure Google OAuth Client Credentials and format routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -211,11 +211,10 @@ router.get("/google", (req, res) => {
     });
   }
 
-  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${
-    env.GOOGLE_CLIENT_ID
-  }&redirect_uri=${encodeURIComponent(
-    env.GOOGLE_REDIRECT_URI
-  )}&response_type=code&scope=openid%20email%20profile`;
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${env.GOOGLE_CLIENT_ID
+    }&redirect_uri=${encodeURIComponent(
+      env.GOOGLE_REDIRECT_URI
+    )}&response_type=code&scope=openid%20email%20profile`;
 
   return res.redirect(authUrl);
 });
